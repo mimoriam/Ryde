6d024d4f54d15c70aeaf14660068a6d17e0a167e
Mimo <mimoriam420@gmail.com>
2024-11-22 03:07:14 +0500

    feat(app): file/folder setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,11 @@ import { Stack } from 'expo-router';
 export default function Layout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Home' }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(root)" options={{ headerShown: false }} />
+
+      <Stack.Screen name="+not-found" />
     </Stack>
   );
 }
